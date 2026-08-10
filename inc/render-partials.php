33c864a6454a01f633c88b365c36c4006ad58ff1
Jasa Kolam Renang Bogor <bintangasia21@gmail.com>
2026-08-10 21:07:26 +0700

    Hapus skrip debug/recovery admin yang sudah tidak dipakai, tambah Artikel ke menu header
    debug-auth-path.php dan recover-admin-2026-08-10.php sudah selesai
    tugasnya (admin berhasil login lagi) -- dihapus supaya tidak jadi celah
    akses tak terpakai (keduanya sengaja tanpa Basic Auth saat dibuat).
    Link /artikel/ ditambahkan ke nav header, sebelumnya cuma ada di footer.

--- inc/render-partials.php
+++ inc/render-partials.php
@@ -113,6 +113,7 @@ function render_header_nav($business) {
       <a href="/layanan/">Layanan</a>
       <a href="/area-layanan/">Area Layanan</a>
       <a href="/portofolio/">Portofolio</a>
+      <a href="/artikel/">Artikel</a>
       <a href="/faq/">FAQ</a>
       <a href="/kontak/">Kontak</a>
     </nav>
